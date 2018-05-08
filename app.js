fb0e33fec6d13b7c45da20d691a6e01a55fd52f5
Ronny Perez <ronnypdev@gmail.com>
2018-05-08 08:53:48 -0400

    hobbits added

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ const humans = characters.querySelector('.humans');
 // Select all humans with querySelectorAl, straight from .humans
 const allHumans = humans.querySelectorAll('li');
 console.log(allHumans);
+
+// The Hobbits
+
+// Select all hobbits with children
+const hobbits = document.querySelector('.hobbits');
+const hobbitsChildren = hobbits.children;
+console.log(hobbitsChildren);
+
+// Select the Merry Hobbit
+const merryTheHobbit = hobbits.querySelectorAll('li');
+const merry = merryTheHobbit[2];
+console.log(merry);
